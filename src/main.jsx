422231d5edcfef17f31d231e90db4f54d0c551e3
tnanhd <trannhatanhttqtd@gmail.com>
2024-05-04 21:41:39 -0400

    Feat: Add login button

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import App from './App.jsx';
 import './index.css';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-const domain = process.env.DOMAIN;
+const domain = "dev-so4ctl6dkj0yqhnd.us.auth0.com";
+const clientId = "6Zn04EhSQT8wwzvO5pNIf3I6SaBKX0Zw";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Auth0Provider domain={domain}>
+  <Auth0Provider domain={domain} 
+  clientId={clientId} 
+  authorizationParams={{
+    redirect_uri: window.location.origin
+  }}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
